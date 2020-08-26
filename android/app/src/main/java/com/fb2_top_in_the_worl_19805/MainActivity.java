44078d412efbf945984f034427ad070e6c1807a5
Crowdbotics <team@crowdbotics.com>
2020-08-26 21:36:30 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fb2_top_in_the_worl_19805/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fb2_top_in_the_worl_19805;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "fb2_top_in_the_worl_19805";
+    }
+}
---
+++ backend/fb2_top_in_the_worl_19805/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fb2_top_in_the_worl_19805 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fb2_top_in_the_worl_19805.settings')
+
+application = get_wsgi_application()
