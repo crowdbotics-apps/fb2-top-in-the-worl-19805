7c2479ffa9b79394513dc8a8d7c8cf4089e5ae21
Crowdbotics <team@crowdbotics.com>
2020-08-26 21:38:13 +0000

    Update models

--- backend/fb2_top_in_the_worl_19805/wsgi.py
+++ backend/fb2_top_in_the_worl_19805/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fb2_top_in_the_worl_19805.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fb2_top_in_the_worl_19805.settings")
 
 application = get_wsgi_application()
